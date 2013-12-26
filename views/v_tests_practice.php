5c507b7a1469376447b71d6efa4aaa0434e3fd8e
unknown <linghong_chen@hotmail.com>
2013-12-26 03:58:12 -0500

    fixed typo

--- views/v_tests_practice.php
+++ views/v_tests_practice.php
@@ -4,8 +4,8 @@
 
 		<div id="practicetitle">
 			<P>The vocabulary of the practice library starts with a, ends with z. It is equally divided into four sections. 
-			<p>Click the "start" button, it will give you a word and ask you to pick up the correct explaination from the four explanation choice listed below the word. </p> 
-			<p>Keep on clicking the "start" button and answering evey question. According to your performance, the site will automactically drops out any words you are familiar with from the practice library, but let you practice more for the words you haven't grasped. The word you made more times of wrong choice, you will meet it more often. By this way, you'll learn a new vocabulary fast.</p>
+			<p>Click the section you want to work with, it will give you a word from this section and ask you to pick up the correct explaination from the four explanation choice listed below the word. </p> 
+			<p>Keep on clicking the "next word" button and answering evey question. According to your performance, the site will automactically drops out any words you are familiar with from the practice library, but let you practice more for the words you haven't grasped. The word you made more times of wrong choice, you will meet it more often. By this way, you'll learn a new vocabulary fast.</p>
 		</div>
 
 		<div id="re_load"></div>
