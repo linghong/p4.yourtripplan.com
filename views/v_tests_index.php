5aac8bf1532b659f0b01fa082fc040e195ce2df8
linghong <username@users.noreply.github.com>
2014-09-23 14:53:39 -0400

    convert more files to responsive pages

--- views/v_tests_index.php
+++ views/v_tests_index.php
@@ -1,19 +1,20 @@
 
-		<article>
-      		<div id="preface">
+
+      		<div id="preface" class="col-lg-12 col-md-12" >
       		The site aims to help high school students increase their vocabulary. It has two sections. A vocabulary test section and a word practice exercise section. The vocabulary test section helps students know the percentage of SAT words they have grasped, while the practice exercise section helps students improve their vocabulary. 
       		</div>
 
-			<div id="title">Let's First Start With A Vocabulary Test</div>
-			
-			<div id="testtitle">
-			The test will give you 20 words and ask you to select the correct explaination from the four explanation choice it gives you. After you finish it, you'll receive information about how many answers are correct.
+			<div id="title" class="col-lg-12 col-md-12" >
+				Let's First Start With A Vocabulary Test
 			</div>
 			
-			<input type ='button' class="start_test" value='Start A Vocabulary Test'><br>
-			<div class="word"></div>
-			<div class="result"></div>
-			<div  id="submit"></div>	
-	
-						
-		</article>
+			<div id="testtitle" class="col-lg-12 col-md-12" >
+				The test will give you 20 words and ask you to select the correct explaination from the four explanation choice it gives you. After you finish it, you'll receive information about how many answers are correct.
+			</div>
+			
+			<input type ='button' class="start_test col-lg-12 col-md-12" value='Start A Vocabulary Test'><br>
+			<div class="word col-lg-offset-1 col-md-offset-1 col-lg-11 col-md-11"> </div> 
+			<div class="result col-lg-12px col-md-12px"></div>
+			<div  id="submit" class="col-lg-12 col-md-12"></div>		
+						 
+
--- views/v_tests_practice.php
+++ views/v_tests_practice.php
@@ -1,21 +1,24 @@
-		<div id="title">
+	<div class ="row">
+		<div id="title" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			Let's Do A Vocabulary Practice Exercise
 		</div>		
-
-		<div id="practicetitle">
-			<P>The vocabulary of the practice library starts with a, ends with z. It is equally divided into four sections. 
+	</div>
+	<div class ="row">
+		<div id="practicetitle" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<P>The vocabulary of the practice library starts with a, ends with z. It is equally divided into four sections.</p> 
 			<p>Click the section you want to work with, it will give you a word from this section and ask you to pick up the correct explaination from the four explanation choice listed below the word. </p> 
 			<p>Keep on clicking the "next word" button and answering evey question. According to your performance, the site will automactically drops out any words you are familiar with from the practice library, but let you practice more for the words you haven't grasped. The word you made more times of wrong choice, you will meet it more often. By this way, you'll learn a new vocabulary fast.</p>
 		</div>
-
-		<div id="re_load"></div>
-
-		<div class="start_practice" id="section1" >Section1</div>
-		<div class="start_practice" id="section2" >Section2</div>
-		<div class="start_practice" id="section3" >Section3</div>
-		<div class="start_practice" id="section4" >Section4</div>
-		<div class="start_practice" id="allsections" >All Sections</div>
-		<div id="nextword"></div>
+	</div>
+	<div id="re_load"></div>
+	<div class="row">
+		<div class="start_practice col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10" id="section1" > Section 1 </div>
+		<div class="start_practice col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10" id="section2" > Section 2 </div>
+		<div class="start_practice col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10" id="section3" > Section 3 </div>
+		<div class="start_practice col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10" id="section4" > Section 4 </div>
+		<div class="start_practice col-lg-offset-4 col-lg-4 col-md-offset-4 col-md-4 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10" id="allsections" > All Sections </div>
+	</div>
+		<div id="nextword" ></div>
 		<div class="practice_word"></div>
-		<div  class="result"></div>	
-		<div  id="practice_submit"><div>						
+		<div class="result"></div>	
+		<div  id="practice_submit><div>						
